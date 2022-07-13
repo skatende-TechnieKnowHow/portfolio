8febe9bb93c0eb394541c4e8845d58f7e419044c
TechnieKnowHow <bantu@localhost.localdomain>
2022-07-13 17:28:55 -0400

    added missing files ignored in gitignore

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/portfolio/jekyll/update/2022/05/12/welcome-to-jekyll.html",
+        "teaser": null
+      },{
+        "title": "A Web Developer's road to resilency",
+        "excerpt":"A road to resilency for a Web Developer to a Full-Stack Engineer continues   This blog post explains the process that I went through to become and educate myself the skills of web development, and enventually transition into a Full-Stack Engineer or Software Engineer.  Starting with Vanilla Javascript.  ","categories": ["javascript"],
+        "tags": [],
+        "url": "/portfolio/javascript/2022/05/14/vanilla-javascript.html",
+        "teaser": null
+      }]
